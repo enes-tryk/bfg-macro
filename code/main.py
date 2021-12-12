1b6e00a18fa63bf135093f85bedf49951980f59d
Enes Tiryakioğlu <hasanenestiryakioglu@gmail.com>
2021-12-13 01:42:41 +0300

    Added position selection

--- code/main.py
+++ code/main.py
@@ -1,39 +1,44 @@
-import keyboard
-import pyautogui
-import time
+import keyboard, pyautogui, time
 
 hit = int(input('\nENTER HIT POINTS:\n> '))
+pos = input('\nSELECT YOUR POSITION: [L/R]\n> ')
 print('\nSTARTING SCRIPT IN 5 SECONDS.\n[HOLD ALT+A TO ABORT]')
 time.sleep(5)
-
 state = True
-while state:
 
+while state:
     while not keyboard.is_pressed('alt+a'):
 
-        # Select the deployable
-        pyautogui.click(x=650, y=700)
+        if pos.lower() == 'l':
+            pyautogui.click(x=630, y=700)
+            pyautogui.click(x=400, y=330)
+            pyautogui.click(x=120, y=330)
+            pyautogui.click(x=530, y=700)
+            pyautogui.click(x=400, y=330, clicks=hit, interval=0.25)
+            pyautogui.click(x=120, y=330, clicks=hit, interval=0.25)
+        
+        elif pos.lower() == 'r':
+            pyautogui.click(x=630, y=700)
+            pyautogui.click(x=950, y=330)
+            pyautogui.click(x=1250, y=330)
+            pyautogui.click(x=530, y=700)
+            pyautogui.click(x=950, y=330, clicks=hit, interval=0.25)
+            pyautogui.click(x=1250, y=330, clicks=hit, interval=0.25)
 
-        # Place the deployable
-        pyautogui.click(x=400, y=350)
-        pyautogui.click(x=100, y=350)
-
-        # Select the fist
-        pyautogui.click(x=550, y=700)
-
-        # Destroy the deployable
-        pyautogui.click(x=400, y=350, clicks=hit, interval=0.21)
-        pyautogui.click(x=100, y=350, clicks=hit, interval=0.21)
-
-    else:
-        state = False
-        CONT = input('\nSCRIPT HALTED. DO YOU WANT TO CONTINUE? [Y/N]\n> ')
-
-        if CONT.lower() == "y":
-            hit = int(input('\nENTER HIT POINTS:\n> '))
-            print('\nSTARTING SCRIPT IN 5 SECONDS.\n[HOLD ALT+A TO ABORT]')
-            time.sleep(5)
-
-            state = True
         else:
             break
+    
+    else:
+        state = False
+        restart = input('\nSCRIPT HALTED. DO YOU WANT TO CONTINUE? [Y/N]\n> ')
+
+        if restart.lower() == "y":
+            hit = int(input('\nENTER HIT POINTS:\n> '))
+            pos = input('\nSELECT YOUR POSITION: [L/R]\n> ')
+            print('\nSTARTING SCRIPT IN 5 SECONDS.\n[HOLD ALT+A TO ABORT]')
+            time.sleep(5)
+            state = True
+        
+        else:
+            break
+            

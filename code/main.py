329cd2b93ebda22ae4d947f964b27cfe54c1390b
Enes Tiryakioğlu <hasanenestiryakioglu@gmail.com>
2021-12-13 03:12:50 +0300

    Added farming range for usability

--- code/main.py
+++ code/main.py
@@ -1,44 +1,57 @@
 import keyboard, pyautogui, time
 
-hit = int(input('\nENTER HIT POINTS:\n> '))
-pos = input('\nSELECT YOUR POSITION: [L/R]\n> ')
-print('\nSTARTING SCRIPT IN 5 SECONDS.\n[HOLD ALT+A TO ABORT]')
+position = int(input('\nSELECT YOUR FARMING POSITION:\n[1] RIGHT TO LEFT\n[2] LEFT TO RIGHT\n> '))
+blocks = int(input('\nSELECT THE RANGE TO FARM IN:\n[1] SINGLE BLOCK\n[2] TWO BLOCKS\n> '))
+durability = int(input('\nENTER THE HIT POINTS OF THE ITEM YOU ARE FARMING:\n> '))
+print('\nSTARTING SCRIPT IN 5 SECONDS...\n[HOLD ALT+A TO ABORT PROGRAM]')
 time.sleep(5)
 state = True
 
 while state:
     while not keyboard.is_pressed('alt+a'):
 
-        if pos.lower() == 'l':
+        if position == 1 and blocks == 1:
+            pyautogui.click(x=630, y=700)
+            pyautogui.click(x=400, y=330)
+            pyautogui.click(x=530, y=700)
+            pyautogui.click(x=400, y=330, clicks=durability, interval=0.25)
+
+        elif position == 1 and blocks == 2:
             pyautogui.click(x=630, y=700)
             pyautogui.click(x=400, y=330)
             pyautogui.click(x=120, y=330)
             pyautogui.click(x=530, y=700)
-            pyautogui.click(x=400, y=330, clicks=hit, interval=0.25)
-            pyautogui.click(x=120, y=330, clicks=hit, interval=0.25)
-        
-        elif pos.lower() == 'r':
+            pyautogui.click(x=400, y=330, clicks=durability, interval=0.25)
+            pyautogui.click(x=120, y=330, clicks=durability, interval=0.25)
+
+        elif position == 2 and blocks == 1:
+            pyautogui.click(x=630, y=700)
+            pyautogui.click(x=950, y=330)
+            pyautogui.click(x=530, y=700)
+            pyautogui.click(x=950, y=330, clicks=durability, interval=0.25)
+
+        elif position == 2 and blocks == 2:
             pyautogui.click(x=630, y=700)
             pyautogui.click(x=950, y=330)
             pyautogui.click(x=1250, y=330)
             pyautogui.click(x=530, y=700)
-            pyautogui.click(x=950, y=330, clicks=hit, interval=0.25)
-            pyautogui.click(x=1250, y=330, clicks=hit, interval=0.25)
+            pyautogui.click(x=950, y=330, clicks=durability, interval=0.25)
+            pyautogui.click(x=1250, y=330, clicks=durability, interval=0.25)
 
         else:
             break
-    
+
     else:
         state = False
         restart = input('\nSCRIPT HALTED. DO YOU WANT TO CONTINUE? [Y/N]\n> ')
 
         if restart.lower() == "y":
-            hit = int(input('\nENTER HIT POINTS:\n> '))
-            pos = input('\nSELECT YOUR POSITION: [L/R]\n> ')
-            print('\nSTARTING SCRIPT IN 5 SECONDS.\n[HOLD ALT+A TO ABORT]')
+            position = int(input('\nSELECT YOUR FARMING POSITION:\n[1] RIGHT TO LEFT\n[2] LEFT TO RIGHT\n> '))
+            blocks = int(input('\nSELECT THE RANGE TO FARM IN:\n[1] SINGLE BLOCK\n[2] TWO BLOCKS\n> '))
+            durability = int(input('\nENTER THE HIT POINTS OF THE ITEM YOU ARE FARMING:\n> '))
+            print('\nSTARTING SCRIPT IN 5 SECONDS...\n[HOLD ALT+A TO ABORT PROGRAM]')
             time.sleep(5)
             state = True
-        
+
         else:
             break
-            

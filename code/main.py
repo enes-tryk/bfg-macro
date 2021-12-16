74e25812b000fa2d0e3898d01db5f4a085a6291f
Enes Tiryakioğlu <hasanenestiryakioglu@gmail.com>
2021-12-16 06:53:33 +0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -16,13 +16,13 @@ while state:
     while not keyboard.is_pressed('alt+a'):
 
         if pos == 1 and aim == 1 and res == 1:
-            pyautogui.click(x=920, y=1020)
+            pyautogui.click(x=1100, y=1020)
             pyautogui.click(x=660, y=500)
             pyautogui.click(x=820, y=1020)
             pyautogui.click(x=660, y=500, clicks=hit, interval=0.25)
 
         elif pos == 1 and aim == 2 and res == 1:
-            pyautogui.click(x=920, y=1020)
+            pyautogui.click(x=1100, y=1020)
             pyautogui.click(x=660, y=500)
             pyautogui.click(x=360, y=500)
             pyautogui.click(x=820, y=1020)
@@ -30,13 +30,13 @@ while state:
             pyautogui.click(x=360, y=500, clicks=hit, interval=0.25)
 
         elif pos == 2 and aim == 1 and res == 1:
-            pyautogui.click(x=920, y=1020)
+            pyautogui.click(x=1100, y=1020)
             pyautogui.click(x=1200, y=500)
             pyautogui.click(x=820, y=1020)
             pyautogui.click(x=1200, y=500, clicks=hit, interval=0.25)
 
         elif pos == 2 and aim == 2 and res == 1:
-            pyautogui.click(x=920, y=1020)
+            pyautogui.click(x=1100, y=1020)
             pyautogui.click(x=1200, y=500)
             pyautogui.click(x=1500, y=500)
             pyautogui.click(x=820, y=1020)
@@ -44,13 +44,13 @@ while state:
             pyautogui.click(x=1500, y=500, clicks=hit, interval=0.25)
 
         elif pos == 1 and aim == 1 and res == 2:
-            pyautogui.click(x=630, y=700)
+            pyautogui.click(x=800, y=700)
             pyautogui.click(x=400, y=330)
             pyautogui.click(x=530, y=700)
             pyautogui.click(x=400, y=330, clicks=hit, interval=0.25)
 
         elif pos == 1 and aim == 2 and res == 2:
-            pyautogui.click(x=630, y=700)
+            pyautogui.click(x=800, y=700)
             pyautogui.click(x=400, y=330)
             pyautogui.click(x=120, y=330)
             pyautogui.click(x=530, y=700)
@@ -58,13 +58,13 @@ while state:
             pyautogui.click(x=120, y=330, clicks=hit, interval=0.25)
 
         elif pos == 2 and aim == 1 and res == 2:
-            pyautogui.click(x=630, y=700)
+            pyautogui.click(x=800, y=700)
             pyautogui.click(x=950, y=330)
             pyautogui.click(x=530, y=700)
             pyautogui.click(x=950, y=330, clicks=hit, interval=0.25)
 
         elif pos == 2 and aim == 2 and res == 2:
-            pyautogui.click(x=630, y=700)
+            pyautogui.click(x=800, y=700)
             pyautogui.click(x=950, y=330)
             pyautogui.click(x=1250, y=330)
             pyautogui.click(x=530, y=700)
@@ -78,7 +78,6 @@ while state:
         restart, state = input('\nSCRIPT HALTED. DO YOU WANT TO CONTINUE? [Y/N]\n> '), False
 
         if restart.lower() == "y":
-            res = int(input('\nSELECT YOUR SCREEN RESOLUTION:\n[1] 1920:1080\n[2] 1280:768\n> '))
             pos = int(input('\nSELECT YOUR FARMING POSITION:\n[1] RIGHT TO LEFT\n[2] LEFT TO RIGHT\n> '))
             aim = int(input('\nSELECT THE RANGE TO FARM IN:\n[1] SINGLE BLOCK\n[2] TWO BLOCKS\n> '))
             hit = int(input('\nENTER THE HIT POINTS OF THE ITEM YOU ARE FARMING:\n> '))
